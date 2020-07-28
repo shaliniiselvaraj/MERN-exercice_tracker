3842100fb9e2a75cb70ee1aed689c14dfda15d3b
shalini1298 <shaliniselvaraj1298@gmail.com>
2020-07-28 21:58:08 +0530

    first commit

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+const exercisesRouter = require('./routes/exercise');
+const usersRouter = require('./routes/users');
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+const uri = process.env.ATLAS_URI;
+
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true,useUnifiedTopology: true  }
+);
+const connection = mongoose.connection;
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+app.use('/exercise', exercisesRouter);
+app.use('/users', usersRouter);
+app.listen(port, () => {
+    console.log(`Server is running on port: ${port}`);
+});
